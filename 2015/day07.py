ad0189ef767ee88744f704983f576bda06ef0de5
Pjoter <59371425+Pjoterro@users.noreply.github.com>
2025-08-01 19:21:34 +0200

    day 7 start

---
+++ 2015/day07.py
@@ -0,0 +1,29 @@
+### test cases:
+test = """"""
+test_result = """"""
+
+### input file path
+input_path = """.\\2015\input_day07.txt""" # specific for windows
+
+### functions
+
+### main 
+# mode = "TASK"
+mode = "TEST"
+
+print("\nMode: " + mode)
+if mode == "TASK":
+    file = open(input_path)
+    input = file.read()
+elif mode == "TEST":
+    input = test
+
+result = 0
+
+if mode == "TASK":
+    print("Task 1: " + str(result))
+    # print("Task 2: " + str(result))
+elif mode == "TEST":
+    print("Test 1: " + str(int(test_result) == result) + "\n    Expected test result 1: " + str(test_result) + "   |   Actual test result 1: " + str(result))
+    # print("Test 2: " + str(int(test_result) == result) + "\n    Expected test result 2: " + str(test_result) + "   |   Actual test result 2: " + str(result))
+print()
