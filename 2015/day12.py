564f9c1c1640bb4535bc076765f9f76ae76d0b59
Pjoter <59371425+Pjoterro@users.noreply.github.com>
2025-08-13 15:05:05 +0200

    Update day12.py

--- 2015/day12.py
+++ 2015/day12.py
@@ -1,12 +1,78 @@
 ### test cases:
-test_input = """[1,2,3]\n{"a":2,"b":4}\n[[[3]]]{"a":{"b":4},"c":-1}{"a":[-1,1]}[-1,{"a":1}][]{}"""
+test_input = """[1,2,3]{"a":2,"b":4}[[[3]]]{"a":{"b":4},"c":-1}{"a":[-1,1]}[-1,{"a":1}][]{}"""
 test_result = """18"""
+test_input_2 = """[1,2,3][1,{"c":"red","b":2},3]{"d":"red","e":[1,2,3,4],"f":5}[1,"red",5]"""
+test_result_2 = """16"""
 
 ### input file path
 input_path = """.\\2015\input_day12.txt""" # specific for windows
 
 ### functions
-#TODO: part2
+def assess_bracket(bracket):
+    result = 0
+    has_red = check_for_red(bracket)
+    subrackets = get_subbrackets(bracket)
+    if has_red:
+        result = 0
+    else:
+        result = sum_bracket(bracket)
+        if len(subrackets) > 0:
+            for subbracket in subrackets:
+                result += assess_bracket(subbracket)
+    return result
+    
+def check_for_red(bracket):
+    i = 0
+    current_level = 0
+    if bracket[0] == "[":
+        return False
+    while i < len(bracket)
+        if bracket[i] == "r" and current_level == 1:
+            if bracket[i+1] == "e" and bracket[i+2] == "d":
+                return True
+        elif bracket[i] == "[" or bracket[i] == "{":
+            current_level += 1
+        elif bracket[i] == "]" or bracket[i] == "}":
+            current_level -= 1
+        i += 1
+    return False
+    
+def get_subbrackets(bracket):
+    result = []
+    i = 0
+    current_level = 0
+    buffor = ""
+    while i < len(bracket):
+        if bracket[i] == "[" or bracket[i] == "{":
+            current_level += 1
+        elif bracket[i] == "]" or bracket[i] == "}":
+            current_level -= 1
+            if current_level == 1:
+                result.append(buffor)
+                buffor = ""
+        if current_level > 1:
+            buffor += bracket[i]
+        i += 1
+    return result
+    
+def sum_bracket(bracket):
+    num_symb = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-"]
+    result = 0
+    i = 0
+    num_flag = False
+    current_level = 0
+    num_str = ""
+    while i < len(bracket):
+        if current_level == 1:
+            if not num_flag:
+                if line[bracket] in num_symb:
+                    num_flag = True
+                    num_str += bracket[i] 
+            else:
+                
+        else:
+            pass
+        i += 1
 
 def add_one_line(line):
     num_symb = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-"]
